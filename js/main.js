28464d787c8c0d38abb9bdda7f214324d705435e
Ahmedpop1 <ahmdhsyno@gmail.com>
2022-04-23 11:14:38 +0200

    done

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 let adwin=document.querySelectorAll(".ads-window div");
 let adwim=document.querySelectorAll(".ads-window div img");
-// let adwin=document.querySelectorAll(".ads-window div")[1];
+let search=document.querySelectorAll(".form-control")[0]
+let sugbox=document.querySelectorAll(".sugul")[0]
 
 let n=1;
 let m=0;
@@ -27,4 +28,41 @@ setTimeout(function(){
 },900)
 },1000)
 },4000)
+let a=['laptop','mobile','personal mob','television','microwave','keyboard','table','keyframe','tattweer'];
+search.addEventListener('keyup',function(){
+      // console.log(search.value);
+    
+    let searched=search.value;
+    let srhsrr=searchitem(a,searched);
+    let text='';
+    if(srhsrr){
+    for(let item of srhsrr){
+        text+=`<li>${item}</li>`;
+    }}
+    else{text=''}
+    sugbox.innerHTML=text;
+    // console.log(text);
 
+
+})
+
+
+/*************************************************************************************** */
+
+
+
+
+
+
+
+
+function searchitem(arr,searchit){
+    searchit.trim();
+    if(searchit.trim()!=''){
+    var re = new RegExp(`\\b${searchit}`,"ig");
+    //   let re=/\b+searched/ig;
+    // arr.forEach(function(ele,index){if(re.test(ele)){if(searchit.trim()!=''){return arr[index]}}})
+   let result= arr.filter(function(ele,index){return (re.test(ele))})
+   return result}
+
+}
